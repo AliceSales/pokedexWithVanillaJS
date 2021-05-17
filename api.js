05575986f313d2e21c12fb379c84407e23a4156c
Alice Sales <programmeralice@gmail.com>
2021-05-17 15:54:42 -0300

    update design

--- api.js
+++ api.js
@@ -13,17 +13,35 @@ async function getPokemonsDataUrl() {
       await getPokemonUrl(i).then(response => response.data)
       .then(pokemons => {
         const majorElement = pokemons.types.map(typePokemon=>typePokemon.type.name)
-        const name = pokemons.name
-
+          let elements = 
+            majorElement[1] ? 
+              `
+              <small>
+                  |
+              </small>
+              <small class="type ${majorElement[1]}">
+                ${majorElement[1]}
+              </small>
+              ` : ` `
           let acc = `
-            <li class="list__item ${majorElement[0]} ${name}">
-              <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemons.id}.png" alt="${pokemons.name}"/>
-              <p class="name">${pokemons.id}.${name}</p>
-              <small class="type">${majorElement.join(' | ')}</small>
+            <li class="list__item">
+              <div class="item__image ${majorElement[0]}">
+                  <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemons.id}.png" alt="${pokemons.name}"/>
+              </div>
+              <div class="item__informations">
+                  <p class="name" onclick="pokemonDetails('${pokemons.name}')">${pokemons.id}.${pokemons.name}</p></a>
+                  <div class="container__type">
+                      <small class="type ${majorElement[0]}">
+                        ${majorElement[0]}
+                      </small>
+                      ${elements}
+                  </div>
+              </div>
             </li>
             `
-            list.innerHTML += acc
-        })
+          list.innerHTML += acc
+          return pokemons.name
+      })
     )
   }
 }
